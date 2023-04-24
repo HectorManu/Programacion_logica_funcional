b975ad252b8ff314a92d4edf3a73b90675c46cbb
HectorManu <le19081442@itmerida.edu.mx>
2023-04-24 13:22:59 -0600

    agregue codigo del sistema experto

--- Sis_Expert/main.py
+++ Sis_Expert/main.py
@@ -1,31 +1,45 @@
 import tkinter as tk
 
-# Crear la ventana principal
-ventana = tk.Tk()
+def question_window(question, option1, option2):
+    def option1_selected():
+        result_label.config(text="Has seleccionado la opción 1")
+        # Aquí puedes mostrar la imagen correspondiente a la opción 1
+    def option2_selected():
+        result_label.config(text="Has seleccionado la opción 2")
+        # Aquí puedes mostrar la imagen correspondiente a la opción 2
+    def answer_text():
+        result_label.config(text="Tu respuesta es: " + answer_entry.get())
+    window = tk.Tk()
+    window.title("Pregunta")
+    question_label = tk.Label(window, text=question)
+    question_label.pack()
+    option1_button = tk.Button(window, text=option1, command=option1_selected)
+    option1_button.pack()
+    option2_button = tk.Button(window, text=option2, command=option2_selected)
+    option2_button.pack()
+    answer_label = tk.Label(window, text="Si ninguna de las opciones aplica, por favor escribe tu respuesta aquí:")
+    answer_label.pack()
+    answer_entry = tk.Entry(window)
+    answer_entry.pack()
+    answer_button = tk.Button(window, text="Enviar respuesta", command=answer_text)
+    answer_button.pack()
+    result_label = tk.Label(window, text="")
+    result_label.pack()
+    window.mainloop()
 
-# Crear una variable para almacenar la opción seleccionada
-opcion_seleccionada = tk.StringVar()
+def ask_question():
+    question_window("¿Cuál es tu opción preferida?", "Opción 1", "Opción 2")
 
-# Crear una lista de opciones
-opciones = ['Opción 1', 'Opción 2', 'Opción 3']
+def show_image():
+    image_window("ruta/a/la/imagen.png")
 
-# Crear una función para imprimir la opción seleccionada
-def imprimir_opcion_seleccionada():
-    print(opcion_seleccionada.get())
+def option1_selected():
+    result_label.config(text="Has seleccionado la opción 1")
+    show_image("ruta/a/la/imagen_opcion1.png")
 
-# Crear una etiqueta para las opciones
-etiqueta_opciones = tk.Label(ventana, text="Seleccione una opción:")
+def option2_selected():
+    result_label.config(text="Has seleccionado la opción 2")
+    show_image("ruta/a/la/imagen_opcion2.png")
 
-# Crear un menú desplegable con las opciones
-menu_opciones = tk.OptionMenu(ventana, opcion_seleccionada, *opciones)
+ask_question()
 
-# Crear un botón para imprimir la opción seleccionada
-boton_imprimir = tk.Button(ventana, text="Imprimir opción seleccionada", command=imprimir_opcion_seleccionada)
-
-# Añadir los widgets a la ventana
-etiqueta_opciones.pack()
-menu_opciones.pack()
-boton_imprimir.pack()
-
-# Mostrar la ventana
-ventana.mainloop()
