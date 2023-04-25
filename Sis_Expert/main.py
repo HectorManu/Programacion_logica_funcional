487e024aea8f1eb9456e5d1ec4b0c979d56356f0
HectorManu <le19081442@itmerida.edu.mx>
2023-04-25 06:36:51 -0600

    agregue cambiso al main de sis expert

--- Sis_Expert/main.py
+++ Sis_Expert/main.py
@@ -1,45 +1,219 @@
 import tkinter as tk
+import pickle
+# from PIL import Image, ImageTk
 
-def question_window(question, option1, option2):
-    def option1_selected():
-        result_label.config(text="Has seleccionado la opción 1")
-        # Aquí puedes mostrar la imagen correspondiente a la opción 1
-    def option2_selected():
-        result_label.config(text="Has seleccionado la opción 2")
-        # Aquí puedes mostrar la imagen correspondiente a la opción 2
-    def answer_text():
-        result_label.config(text="Tu respuesta es: " + answer_entry.get())
-    window = tk.Tk()
-    window.title("Pregunta")
-    question_label = tk.Label(window, text=question)
-    question_label.pack()
-    option1_button = tk.Button(window, text=option1, command=option1_selected)
-    option1_button.pack()
-    option2_button = tk.Button(window, text=option2, command=option2_selected)
-    option2_button.pack()
-    answer_label = tk.Label(window, text="Si ninguna de las opciones aplica, por favor escribe tu respuesta aquí:")
-    answer_label.pack()
-    answer_entry = tk.Entry(window)
-    answer_entry.pack()
-    answer_button = tk.Button(window, text="Enviar respuesta", command=answer_text)
-    answer_button.pack()
-    result_label = tk.Label(window, text="")
-    result_label.pack()
-    window.mainloop()
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
 
-def ask_question():
-    question_window("¿Cuál es tu opción preferida?", "Opción 1", "Opción 2")
+    def on_question1_respuesta_changed(self, *args):
+        print(f"Seleccionaste: {self.question1_respuesta.get()}")
+        if self.question1_respuesta.get() == "Sí":
+            print("es si")
+        else:
+            print("es no")
 
-def show_image():
-    image_window("ruta/a/la/imagen.png")
+        return self.question1_respuesta.get()
 
-def option1_selected():
-    result_label.config(text="Has seleccionado la opción 1")
-    show_image("ruta/a/la/imagen_opcion1.png")
+        # self.descripcion_label = tk.Label(self, text="Descripción:")
+        # self.descripcion_label.pack(side="top")
+        # self.descripcion_text = tk.Text(self, height=5, width=50)
+        # self.descripcion_text.pack()
 
-def option2_selected():
-    result_label.config(text="Has seleccionado la opción 2")
-    show_image("ruta/a/la/imagen_opcion2.png")
+        # self.ruta_label = tk.Label(self, text="Ruta de imagen:")
+        # self.ruta_label.pack(side="top")
+        # self.ruta_text = tk.Entry(self)
+        # self.ruta_text.pack()
 
-ask_question()
+        # self.guardar_boton = tk.Button(self, text="Guardar", command=self.guardar_info)
+        # self.guardar_boton.pack(side="bottom")
 
+    # def guardar_info(self):
+    #     descripcion = self.descripcion_text.get("1.0", tk.END)
+    #     ruta = self.ruta_text.get()
+    #     print(f"Descripción: {descripcion}")
+    #     print(f"Ruta de imagen: {ruta}")
+
+    #     archivo = "datos.pickle"
+    #     try:
+    #         with open(archivo, "rb") as f:
+    #             datos = pickle.load(f)
+    #     except FileNotFoundError:
+    #         datos = {}
+
+    #     # Agregamos la información al diccionario
+    #     datos["descripcion"] = descripcion
+    #     datos["ruta_imagen"] = ruta
+
+    #     # Guardamos el diccionario en el archivo
+    #     with open(archivo, "wb") as f:
+    #         pickle.dump(datos, f)
+
+    #     # Mostramos la información guardada
+    #     print(datos)
+
+    #     # Eliminamos los widgets de la ventana
+    #     self.descripcion_label.destroy()
+    #     self.descripcion_text.destroy()
+    #     self.ruta_label.destroy()
+    #     self.ruta_text.destroy()
+    #     self.guardar_boton.destroy()
+
+
+    def validacion(self):
+        print(self.on_question1_respuesta_changed)
+
+    def on_question2_respuesta_changed(self, *args):
+        # print(f"Seleccionaste: {self.question2_respuesta.get()}")
+
+        if self.question2_respuesta.get() == "Menor de edad":
+            print("menor")
+        elif self.question2_respuesta.get() == "Joven o adolescente":
+            print("joven")
+        elif self.question2_respuesta.get() == "Adulto responsable":
+            print("adulto")
+            
+
+
+        # COMO IMPRIMIR IMAGEN Y TEXTO PARA EL RESULTADO FINAL 
+
+        # imagen = Image.open("ruta/a/la/imagen.png")
+        # imagen = imagen.resize((300, 300))
+        # imagen_tk = ImageTk.PhotoImage(imagen)
+
+        # imagen_label = tk.Label(self, image=imagen_tk)
+        # imagen_label.pack()
+
+        # texto_label = tk.Label(self, text="Texto encima de la imagen")
+        # texto_label.place(relx=0.5, rely=0.1, anchor="center")
+
+    def on_question3_respuesta_changed(self, *args):
+        # print(f"Seleccionaste: {self.question3_respuesta.get()}")
+        if self.question3_respuesta.get() == "Falla":
+            print("falla")
+        elif self.question3_respuesta.get() == "Estable":
+            print("estable")
+        elif self.question3_respuesta.get() == "Excelente":
+            print("Exclente")
+
+    def on_question4_respuesta_changed(self, *args):
+        # print(f"Seleccionaste: {self.question4_respuesta.get()}")
+        if self.question4_respuesta.get() == "Más de 4.5":
+            print("mas de 4.5")
+        elif self.question4_respuesta.get() == "Entre 4 y 4.5":
+            print("entre")
+        elif self.question4_respuesta.get() == "Menos de 4":
+            print("menos")
+
+    def on_question5_respuesta_changed(self, *args):
+        # print(f"Seleccionaste: {self.question5_respuesta.get()}")
+        if self.question5_respuesta.get() == "Juegos":
+            print("juegos")
+        elif self.question5_respuesta.get() == "Educación":
+            print("educacion")
+
+    def create_widgets(self):
+
+        # Pregunta 1
+
+        self.question1_respuesta = tk.StringVar(value="Ninguno")
+        self.question1_respuesta.trace("w", self.on_question1_respuesta_changed)
+
+        self.question1_label = tk.Label(self, text="¿Prefieres una aplicación gratuita o estás dispuesto a pagar por características adicionales?")
+        self.question1_label.pack(side="top")
+
+        self.question1_respuesta_azul = tk.Radiobutton(self, text="Sí", variable=self.question1_respuesta, value="Sí")
+        self.question1_respuesta_azul.pack(anchor="center")
+
+        self.question1_respuesta_rojo = tk.Radiobutton(self, text="No", variable=self.question1_respuesta, value="No")
+        self.question1_respuesta_rojo.pack(anchor="center")
+
+        self.question1_respuesta_label = tk.Label(self, textvariable=self.question1_respuesta)
+        # self.question1_respuesta_label.pack(side="bottom")
+        self.validacion()
+
+        # Pregunta 2
+        self.question2_respuesta = tk.StringVar(value="Ninguno")
+        self.question2_respuesta.trace("w", self.on_question2_respuesta_changed)
+
+        self.question2_label = tk.Label(self, text="¿Qué edad tiene el usuario?")
+        self.question2_label.pack(side="top")
+
+        self.question2_respuesta_azul = tk.Radiobutton(self, text="Menor de edad", variable=self.question2_respuesta, value="Menor de edad")
+        self.question2_respuesta_azul.pack(anchor="center")
+
+        self.question2_respuesta_rojo = tk.Radiobutton(self, text="Juven o adolescente", variable=self.question2_respuesta, value="Joven o adolescente")
+        self.question2_respuesta_rojo.pack(anchor="center")
+
+        self.question2_respuesta_rojo = tk.Radiobutton(self, text="Adulto responsable", variable=self.question2_respuesta, value="Adulto responsable")
+        self.question2_respuesta_rojo.pack(anchor="center")
+
+        self.question2_respuesta_label = tk.Label(self, textvariable=self.question2_respuesta)
+        # self.question2_respuesta_label.pack(side="bottom")
+
+
+        # # Pregunta 3
+        self.question3_respuesta = tk.StringVar(value="Ninguno")
+        self.question3_respuesta.trace("w", self.on_question3_respuesta_changed)
+
+        self.question3_label = tk.Label(self, text="¿Qué tan estable es tu internet?")
+        self.question3_label.pack(side="top")
+
+        self.question3_respuesta_azul = tk.Radiobutton(self, text="Falla", variable=self.question3_respuesta, value="Falla")
+        self.question3_respuesta_azul.pack(anchor="center")
+
+        self.question3_respuesta_rojo = tk.Radiobutton(self, text="Estable", variable=self.question3_respuesta, value="Estable")
+        self.question3_respuesta_rojo.pack(anchor="center")
+
+        self.question3_respuesta_rojo = tk.Radiobutton(self, text="Excelente", variable=self.question3_respuesta, value="Excelente")
+        self.question3_respuesta_rojo.pack(anchor="center")
+
+        self.question3_respuesta_label = tk.Label(self, textvariable=self.question3_respuesta)
+        # self.question3_respuesta_label.pack(side="bottom")
+
+        # # Pregunta 4
+        self.question4_respuesta = tk.StringVar(value="Ninguno")
+        self.question4_respuesta.trace("w", self.on_question4_respuesta_changed)
+
+        self.question4_label = tk.Label(self, text="¿Cuántas estrellas tendría que tener la aplicación para que la descargues, considerando que 5 es el más alto?")
+        self.question4_label.pack(side="top")
+
+        self.question4_respuesta_azul = tk.Radiobutton(self, text="Menos de 4", variable=self.question4_respuesta, value="Menos de 4")
+        self.question4_respuesta_azul.pack(anchor="center")
+
+        self.question4_respuesta_rojo = tk.Radiobutton(self, text="Entre 4 y 4.5", variable=self.question4_respuesta, value="Entre 4 y 4.5")
+        self.question4_respuesta_rojo.pack(anchor="center")
+
+        self.question4_respuesta_rojo = tk.Radiobutton(self, text="Más de 4.5", variable=self.question4_respuesta, value="Más de 4.5")
+        self.question4_respuesta_rojo.pack(anchor="center")
+
+        self.question4_respuesta_label = tk.Label(self, textvariable=self.question4_respuesta)
+        # self.question4_respuesta_label.pack(side="bottom")
+
+        # Pregunta 5
+        self.question5_respuesta = tk.StringVar(value="Ninguno")
+        self.question5_respuesta.trace("w", self.on_question5_respuesta_changed)
+
+        self.question5_label = tk.Label(self, text="¿Qué tipo de categoría buscas?")
+        self.question5_label.pack(side="top")
+
+        self.question5_respuesta_azul = tk.Radiobutton(self, text="Juegos", variable=self.question5_respuesta, value="Juegos")
+        self.question5_respuesta_azul.pack(anchor="center")
+
+        self.question5_respuesta_rojo = tk.Radiobutton(self, text="Educación", variable=self.question5_respuesta, value="Educación")
+        self.question5_respuesta_rojo.pack(anchor="center")
+
+        self.question5_respuesta_label = tk.Label(self, textvariable=self.question5_respuesta)
+        # self.question5_respuesta_label.pack(side="bottom")
+
+
+        # Botón de salida
+        # self.quit = tk.Button(self, text="Salir", fg="red", command=self.master.destroy)
+        # self.quit.pack(side="left")
+
+root = tk.Tk()
+app = Application(master=root)
+app.mainloop()
