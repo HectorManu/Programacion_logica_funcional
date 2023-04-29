65a8b3cf291d11dedac74d9eaaec7349c334d417
HectorManu <le19081442@itmerida.edu.mx>
2023-04-28 22:02:54 -0600

    agregue cambios en las preguntas y ahora lo que hace falta es agregar

--- Sis_Expert/main.py
+++ Sis_Expert/main.py
@@ -58,13 +58,6 @@ class Application(tk.Frame):
             self.respuesta5 = "Educación"
             self.validacion()
 
-    
-    # def guardar_info(self):
-        
-    
-    
-    
-    
     def validacion(self):
         archivo = "datos.pickle"
         try:
@@ -73,120 +66,42 @@ class Application(tk.Frame):
         except FileNotFoundError:
             datos = {}
 
-
-        # for clave, valor in datos.items():
-        #     if isinstance(valor, list) and len(valor) > 0:
-        #         print(valor[1])
-        #     else:
-        #         print("El valor para la clave {} no es una lista o está vacío".format(clave))
-        #         print("los valores son ",valor[clave])
-
-        
-        
-        
         # COMPROBAR SI ESTA LO QUE BUSCO DE ACUERDO A LOS PARÁMETROS 
         for clave, valor in datos.items():
             # print("AQUÍ SALE ESTA MMDA",valor[0])
             if valor[0] == self.respuesta1:
                 print(valor[0])
-                None
-            #     if valor[1] == self.respuesta2:
-            #         None
-            #         if valor[2] == self.respuesta3:
-            #             None
-            #             if valor[3] == self.respuesta4:
-            #                 None
-            #                 if valor[4] == self.respuesta5:
-            #                     # SI LLEGO ENTONCES PUES SÍ ESTÁ XD
-            #                     # COMO IMPRIMIR IMAGEN Y TEXTO PARA EL RESULTADO FINAL 
-            
-            #                     # imagen = Image.open("ruta/a/la/imagen.png")
-            #                     # imagen = imagen.resize((300, 300))
-            #                     # imagen_tk = ImageTk.PhotoImage(imagen)
-
-            #                     # imagen_label = tk.Label(self, image=imagen_tk)
-            #                     # imagen_label = tk.Label(self)
-            #                     # imagen_label.pack()
-
-            #                     texto_label = tk.Label(self, text=clave)
-            #                     texto_label.place(relx=0.5, rely=0.1, anchor="center")
-
-            #                     texto_label = tk.Label(self, text=valor[5])
-            #                     texto_label.place(relx=0.5, rely=0.1, anchor="center")
-
-            #                     # Crear un botón
-            #                     button = tk.Button(self, text="Mostrar mensaje", command=lambda: messagebox.showinfo("Mensaje", "¡Hola mundo!"))
-
-            #                     # Ubicar el botón al final de la ventana
-            #                     button.place(side="bottom")
-            #                     None
-            #                 else:
-            #                     # NO HAY
-            #                     ## METODO PARA 
-            #                     None
-            #             else:
-            #                 # NO HAY
-            #                 None
-            #         else:
-            #             # NO HAY
-            #             None
-            #     else:
-            #         # NO HAY
-            #         None
+                if valor[1] == self.respuesta2:
+                    print(valor[1])
+                    if valor[2] == self.respuesta3:
+                        print(valor[2])
+                        if valor[3] == self.respuesta4:
+                            print(valor[3])
+                            if valor[4] == self.respuesta5:
+                                print(valor[4])
+                                print(f'El juego que consultaste es: {clave}')
+                                print(f'La descripción es: {valor[5]}')
+                                print(f'La ruta de la imagen es: {valor[6]}')
+                            else:
+                                ## METODO PARA 
+                                print(f"No coincidio con {self.respuesta5}")
+                        else:
+                            print(f"No coincidio con {self.respuesta4}")
+                    else:
+                        print(f"No coincidio con {self.respuesta3}")
+                else:
+                    print(f"No coincidio con {self.respuesta2}")
             else:
-                # NO HAY
-                print("PUES NO HAY PERRO")
-                None
-
-
+                print(f"No coincidio con {self.respuesta1}")
 
         # SI NO HAY ENTONCES LLAMAR A LA FUNCIÓN QUE PONE EL TEXTO Y AGREGAR __NOMBRE__, __DESCRIPCIÓN__, __RUTA DE IMAGEN__
         
-        
-        # DE ESTAR LO QUE BUSCO IMPRIMO UNA IMAGEN Y UN BOTON QUE DESPLIEGA UNA SUBVENTANA FLOTANTE LA CUAL DA UNA EXPLICACIÓN 
+        # DE ESTAR LO QUE BUSCO IMPRIMO UNA IMAGEN Y UN BOTON QUE DESPLIEGA UNA SUBVENTANA FLOTANTE LA CUAL DA UNA EXPLICACIÓN
 
-
-        
-        # descripcion = "Juego de mundo abierto, supervivencia y construcción que se puede jugar de un solo jugador o multi"
-        # ruta = "/minecraft.jpeg"
-        # nombre = "Minecraft"
-        # atributos = [self.respuesta1,self.respuesta2,self.respuesta3,self.respuesta4,self.respuesta5,descripcion,ruta]
-        
-
-
-
-        # # Agregamos la información al diccionario
-        # datos[nombre] = atributos
-
-        # # Guardamos el diccionario en el archivo
-        # with open(archivo, "wb") as f:
-        #     pickle.dump(datos, f)
-
-        
-        # self.descripcion_label = tk.Label(self, text="Descripción:")
-        # self.descripcion_label.pack(side="top")
-        # self.descripcion_text = tk.Text(self, height=5, width=50)
-        # self.descripcion_text.pack()
-
-        # self.ruta_label = tk.Label(self, text="Ruta de imagen:")
-        # self.ruta_label.pack(side="top")
-        # self.ruta_text = tk.Entry(self)
-        # self.ruta_text.pack()
-
-        # self.guardar_boton = tk.Button(self, text="Guardar", command=self.guardar_info)
-        # self.guardar_boton.pack(side="bottom")
-
-
-
-
-
+    def guardar_datos(self):
+        print("Estoy guardando cosas")
 
     def create_widgets(self):
-        # Crear un botón
-        # button = tk.Button(self, text="Mostrar mensaje", command=lambda: messagebox.showinfo("Mensaje", "¡Hola mundo!"))
-
-        # Ubicar el botón al final de la ventana
-        # button.pack(side="bottom")
         # Pregunta 1
 
         self.question1_respuesta = tk.StringVar(value="Ninguno")
@@ -202,8 +117,6 @@ class Application(tk.Frame):
         self.question1_respuesta_rojo.pack(anchor="center")
 
         self.question1_respuesta_label = tk.Label(self, textvariable=self.question1_respuesta)
-        # self.question1_respuesta_label.pack(side="bottom")
-        
 
         # Pregunta 2
         self.question2_respuesta = tk.StringVar(value="Ninguno")
@@ -215,15 +128,13 @@ class Application(tk.Frame):
         self.question2_respuesta_azul = tk.Radiobutton(self, text="Menor de edad", variable=self.question2_respuesta, value="Menor de edad")
         self.question2_respuesta_azul.pack(anchor="center")
 
-        self.question2_respuesta_rojo = tk.Radiobutton(self, text="Juven o adolescente", variable=self.question2_respuesta, value="Joven o adolescente")
+        self.question2_respuesta_rojo = tk.Radiobutton(self, text="Joven o adolescente", variable=self.question2_respuesta, value="Joven o adolescente")
         self.question2_respuesta_rojo.pack(anchor="center")
 
         self.question2_respuesta_rojo = tk.Radiobutton(self, text="Adulto responsable", variable=self.question2_respuesta, value="Adulto responsable")
         self.question2_respuesta_rojo.pack(anchor="center")
 
         self.question2_respuesta_label = tk.Label(self, textvariable=self.question2_respuesta)
-        # self.question2_respuesta_label.pack(side="bottom")
-
 
         # # Pregunta 3
         self.question3_respuesta = tk.StringVar(value="Ninguno")
@@ -242,9 +153,8 @@ class Application(tk.Frame):
         self.question3_respuesta_rojo.pack(anchor="center")
 
         self.question3_respuesta_label = tk.Label(self, textvariable=self.question3_respuesta)
-        # self.question3_respuesta_label.pack(side="bottom")
 
-        # # Pregunta 4
+        # Pregunta 4
         self.question4_respuesta = tk.StringVar(value="Ninguno")
         self.question4_respuesta.trace("w", self.on_question4_respuesta_changed)
 
@@ -261,7 +171,6 @@ class Application(tk.Frame):
         self.question4_respuesta_rojo.pack(anchor="center")
 
         self.question4_respuesta_label = tk.Label(self, textvariable=self.question4_respuesta)
-        # self.question4_respuesta_label.pack(side="bottom")
 
         # Pregunta 5
         self.question5_respuesta = tk.StringVar(value="Ninguno")
@@ -277,12 +186,7 @@ class Application(tk.Frame):
         self.question5_respuesta_rojo.pack(anchor="center")
 
         self.question5_respuesta_label = tk.Label(self, textvariable=self.question5_respuesta)
-        # self.question5_respuesta_label.pack(side="bottom")
-
-
-        # Botón de salida
-        # self.quit = tk.Button(self, text="Salir", fg="red", command=self.master.destroy)
-        # self.quit.pack(side="left")
+        
 
 root = tk.Tk()
 app = Application(master=root)
